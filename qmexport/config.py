7f500b770b093f726582ca45c4685314c2f289e3
Bill Jameson <williamjameson@alum.rpi.edu>
2016-09-30 11:31:16 -0400

    Add Part CSV headers to config

--- qmexport/config.py
+++ qmexport/config.py
@@ -1,6 +1,7 @@
+output_path = 'output/'
 csv_path = 'ref/'
-csv_files = {'part': '20160929_1646_qm_part_query.csv',
-             'part price': '20160929_1654_qm_part_prices_query.csv',
+csv_files = {'part': '20160930_1036_qm_part_query.csv',
+             'part price': '20160930_1048_qm_part_prices_query.csv',
              'part revision': '',
              'bill of materials': '',
              'bill of operations': ''}
@@ -25,3 +26,10 @@ sn_base_data_type = 'MASK'
 sn_mask = 'NF'
 sn_mask_example = 'NF9999999'
 net_weight_uom = 'LB'
+
+# CSV headers
+part_header = (
+        'Company,PartNum,SearchWord,PartDescription,ClassID,IUM,PUM,'
+        'TypeCode,PricePerCode,ProdCode,SalesUM,UsePartRev,SNFormat,'
+        'SNBaseDataType,SNMask,SNMaskExample,UOMClassID,NetWeightUOM')
+part_price_header = 'Company,PartNum,PartDescription,UnitPrice'
