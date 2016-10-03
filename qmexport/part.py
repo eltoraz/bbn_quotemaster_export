3bd9ce80ac7e0fbc2fd3f93b4ca42964ca4b2071
Bill Jameson <williamjameson@alum.rpi.edu>
2016-10-03 14:31:07 -0400

    Switch to OrderedSets
    For easier reading of all-in-one data output
    Note: this is a third-party lib (`orderedset` on pypi)

--- qmexport/part.py
+++ qmexport/part.py
@@ -1,5 +1,6 @@
 """A part, as it exists in Epicor ERP
 """
+from orderedset import OrderedSet
 from qmexport import config
 
 class Part(object):
@@ -12,14 +13,15 @@ class Part(object):
     """
     # these are the fields that will be passed to DMT
     #  in various iterations
-    expected_fields = set.union(
-            set(config.part_header.split(',')),
-            set(config.part_price_header.split(',')),
-            set(config.part_plant_header.split(',')),
-            set(config.part_rev_header.split(',')))
+    expected_fields = list(OrderedSet(config.part_header.split(',')).union(
+            OrderedSet(config.part_plant_header.split(',')).union(
+            OrderedSet(config.part_rev_header.split(',')))))
 
     def __init__(self, dictionary):
         self.update(dictionary)
+
+    def __repr__(self):
+        return self.__dict__.__repr__()
 
     def update(self, dictionary):
         """Set the instance part's fields based on the passed-in
