70ae8505305e40e5560594237fe5b4465f6a6e9a
Bill Jameson <williamjameson@alum.rpi.edu>
2016-10-10 16:05:43 -0400

    Create output directory if it doesn't exist already

--- qmexport/write_data.py
+++ qmexport/write_data.py
@@ -2,12 +2,17 @@
 format to a CSV for use with Epicor DMT
 """
 import csv
+import os
 
 def write_csv(fieldnames, data, filename):
     """Write a CSV file at `filename` with headers (and order)
     specified by `fieldnames` and body in `data` (a list of dicts
     with keys matching the fields)
     """
+    # create the output directory if it doesn't already exist
+    if not os.path.isdir(os.path.dirname(filename)):
+        os.mkdirs(os.path.dirname(filename))
+
     with open(filename, 'w', encoding='iso8859_15') as csv_file:
         csv_writer = csv.DictWriter(csv_file,
                                     extrasaction='ignore',
