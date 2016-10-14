4adf8d076951a347f01e396d6798dc09fb33a420
Bill Jameson <williamjameson@alum.rpi.edu>
2016-10-14 15:57:16 -0400

    Catch KeyErrors when trying to add revisions
    Mismatch between data from Quote Master and existing files

--- qmexport/map_data/map_rev.py
+++ qmexport/map_data/map_rev.py
@@ -12,6 +12,11 @@ def map_rev(data):
 def update_parts(part_data, rev_dict):
     """Update the part data with newly-entered revision numbers
     """
+    count = 0
     for part in rev_dict:
+        if part not in part_data:
+            count += 1
+            continue
+
         part_data[part].update({'RevisionNum': rev_dict[part],
                                 'RevShortDesc': 'Revision ' + rev_dict[part]})
