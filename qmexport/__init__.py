2a4ded6931a51f467f29cdb08ce911054df7371d
Bill Jameson <williamjameson@alum.rpi.edu>
2016-10-14 16:06:37 -0400

    Fix data structure passed to write_data
    Was trying to pass an unsupported dict for missing part revs

--- qmexport/__init__.py
+++ qmexport/__init__.py
@@ -31,8 +31,8 @@ def debug(test_single_pn):
     missing_revs = [pn for pn in dmt_part_data.keys() if _missing_req_rev(pn)]
     print(len(missing_revs), 'parts need revision numbers manually specified')
     write_data.write_csv(['PartNum', 'RevisionNum'],
-                         missing_revs,
-                         config.output_path+'missing_part_revs.csv')
+                          [{'PartNum': pn, 'RevisionNum': missing_revs[pn]} for pn in missing_revs],
+                          config.output_path+'missing_part_revs.csv')
 
     print('---BOM---')
     print(len(bom_data), 'BOM entries read in')
